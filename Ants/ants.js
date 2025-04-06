a55150d0d68ae33faf66684282ddb2653bd96257
dmitryLem <dlemesonok0@gmail.com>
2025-04-06 22:32:23 +0700

    added obstacles, change probability and I want to be an ant

--- Ants/ants.js
+++ Ants/ants.js
@@ -1,4 +1,4 @@
-import {correctPos, delay, drawRect, generateColony, updatePixel} from './help.js'
+import {colors, correctPos, delay, drawRect, generateColony, updatePixel} from './help.js'
 
 let canvasHeight = 100;
 let canvasWidth = 100;
@@ -9,7 +9,7 @@ let d = [[-1, 0], [0, -1], [1, 0], [0, 1], [-1, -1], [1, -1], [1, 1], [-1, 1]];
 
 let coef_transpire = 0.001;
 
-let alpha = 3;
+let alpha = 5;
 let beta = 1;
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -17,8 +17,10 @@ document.addEventListener('DOMContentLoaded', () => {
     let ctx = canvas.getContext('2d');
     let startButton = document.getElementById('startButton');
     let foodButton = document.getElementById('foodButton');
+    let obstacleButton = document.getElementById('obstacleButton');
 
     let mode = 'colony';
+    let isDrawingObstacle = false;
     let colony = [];
     let matrix = Array.from({ length: canvasHeight }, () =>
         Array.from({ length: canvasWidth }, () => ({
@@ -26,7 +28,8 @@ document.addEventListener('DOMContentLoaded', () => {
             pheromones_home: 0,
             food: 0,
             ants: 0,
-            colony: false
+            colony: false,
+            obstacle: false
         }))
     );
     let ants = [];
@@ -36,11 +39,33 @@ document.addEventListener('DOMContentLoaded', () => {
     canvas.addEventListener('click', (event) => {
         if (mode === 'colony') addColony(event);
         if (mode === 'food') addFood(event);
+        if (mode === 'obstacle') addObstacle(event);
     })
 
     foodButton.addEventListener('click', () => {
         mode = 'food';
     })
+
+    obstacleButton.addEventListener('click', () => {
+        mode = 'obstacle';
+    })
+
+    canvas.addEventListener('mousedown', (event) => {
+        if (mode === 'obstacle') {
+            isDrawingObstacle = true;
+            addObstacle(event);
+        }
+    });
+
+    canvas.addEventListener('mousemove', (event) => {
+        if (isDrawingObstacle && mode === 'obstacle') {
+            addObstacle(event);
+        }
+    });
+
+    canvas.addEventListener('mouseup', () => {
+        isDrawingObstacle = false; // Остановить рисование при отпускании кнопки мыши
+    });
 
     startButton.addEventListener('click', () => {
         if (colony.length === 0) return;
@@ -59,7 +84,7 @@ document.addEventListener('DOMContentLoaded', () => {
         let x = Math.floor(event.offsetX * scaleX);
         let y = Math.floor(event.offsetY * scaleY);
         let colony_size = 4;
-        let ch = 1;
+        let ch = 2;
 
         if (
             x >= colony_size / 2  && x + colony_size / 2 <= canvasWidth &&
@@ -108,6 +133,28 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    function addObstacle(event) {
+        let scaleX = canvas.width / canvas.clientWidth;
+        let scaleY = canvas.height / canvas.clientHeight;
+
+        let x = Math.floor(event.offsetX * scaleX);
+        let y = Math.floor(event.offsetY * scaleY);
+
+        let ch = 1;
+
+        if (x >= 0 && x < canvasWidth && y >= 0 && y < canvasHeight) {
+            drawRect(ctx, x, y, 'gray', 1);
+            for (let dy = -ch; dy < ch; dy++) {
+                for (let dx =  - ch; dx < ch; dx++) {
+                    const currentX = x + dx;
+                    const currentY = y + dy;
+                    updatePixel(ctx, matrix, x, y);
+                    matrix[currentX][currentY].obstacle = true;
+                }
+            }
+        }
+    }
+
     function isFood(x, y) {
         return matrix[x][y].food > 0;
     }
@@ -130,13 +177,16 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function nextCeil(ant) {
-        let probabilities = [];
+        let probabilities = Array.from({ length: 8 }, () => ({
+            direction: 0,
+            value: 0
+        }));
         let total = 0;
         for (let i = 0; i < d.length; i++) {
             let newX = ant.x + d[i][0];
             let newY = ant.y + d[i][1];
 
-            if (newX >= 0 && newX < canvasWidth && newY >= 0 && newY < canvasHeight) {
+            if (newX >= 0 && newX < canvasWidth && newY >= 0 && newY < canvasHeight && !matrix[newX][newY].obstacle) {
                 let pheromones = 0;
                 let heuristic = 0;
 
@@ -146,26 +196,41 @@ document.addEventListener('DOMContentLoaded', () => {
 
                 if (ant.state === 'search') {
                     pheromones = matrix[newX][newY].pheromones_food;
-
+                    ant.steps += 1;
                     heuristic = 1;
                 } else if (ant.state === 'food') {
                     pheromones = matrix[newX][newY].pheromones_home;
-
-                    let distanceToColony = Math.abs(newX - colony[0]) + Math.abs(newY - colony[1]);
-                    heuristic = 1 / (distanceToColony + 1);
-
-                    let angleHeuristic = calculateAngleHeuristic(ant, newX, newY);
-                    heuristic *= angleHeuristic;
+                    ant.steps += 1;
+                    // let distanceToColony = Math.abs(newX - colony[0]) + Math.abs(newY - colony[1]);
+                    // heuristic = 1 / (distanceToColony + 1);
+                    //
+                    // let angleHeuristic = calculateAngleHeuristic(ant, newX, newY);
+                    // heuristic *= angleHeuristic;
+                    heuristic = 1;
                 }
 
                 let probability = Math.pow(pheromones, alpha) * Math.pow(heuristic, beta);
-                probabilities.push({ direction: i, value: probability });
+                probabilities[i] = { direction: i, value: probability };
                 total += probability;
             }
         }
 
         if (total === 0) {
-            return Math.floor(Math.random() * d.length);
+            let availableDirections = [];
+            for (let i = 0; i < d.length; i++) {
+                let newX = ant.x + d[i][0];
+                let newY = ant.y + d[i][1];
+
+                if (newX >= 0 && newX < canvasWidth && newY >= 0 && newY < canvasHeight && !matrix[newX][newY].obstacle) {
+                    availableDirections.push(i);
+                }
+            }
+
+            if (availableDirections.length > 0) {
+                return availableDirections[Math.floor(Math.random() * availableDirections.length)];
+            }
+
+            return -1;
         }
 
         let cumulativeProbability = 0;
@@ -198,7 +263,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
             ants[i].memory.push({ x: ants[i].x, y: ants[i].y });
 
-            if (ants[i].memory.length > 10) {
+            if (ants[i].memory.length > 20) {
                 ants[i].memory.shift();
             }
 
@@ -206,12 +271,14 @@ document.addEventListener('DOMContentLoaded', () => {
                 ants[i].nutrition = matrix[ants[i].x][ants[i].y].food;
                 ants[i].state = 'food';
                 ants[i].memory = [];
+                ants[i].steps = 1;
             }
             if (isColony(ants[i].x, ants[i].y))
             {
                 ants[i].nutrition = 100;
                 ants[i].state = 'search';
                 ants[i].memory = [];
+                ants[i].steps = 1;
             }
 
             matrix[ants[i].x][ants[i].y].ants++;
@@ -220,18 +287,18 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function transpirePheromones() {
-        for (const cord in pheromonesHome) {
-            pheromonesHome[cord].pheromones = pheromonesHome[cord].pheromones * (1 - 0.001);
-            matrix[pheromonesHome[cord].x][pheromonesHome[cord].y].pheromones_home = pheromonesHome[cord].pheromones;
-            updatePixel(ctx, matrix, pheromonesHome[cord].x, pheromonesHome[cord].y);
-            if (pheromonesHome[cord].pheromones <= 0) {
-                matrix[pheromonesHome[cord].x][pheromonesHome[cord].y].pheromones_home = 0;
-                updatePixel(ctx, matrix, pheromonesHome[cord].x, pheromonesHome[cord].y);
-                delete pheromonesHome[cord];
-            }
-        }
+        // for (const cord in pheromonesHome) {
+        //     pheromonesHome[cord].pheromones = pheromonesHome[cord].pheromones * (1 - 0.001);
+        //     matrix[pheromonesHome[cord].x][pheromonesHome[cord].y].pheromones_home = pheromonesHome[cord].pheromones;
+        //     updatePixel(ctx, matrix, pheromonesHome[cord].x, pheromonesHome[cord].y);
+        //     if (pheromonesHome[cord].pheromones <= 0) {
+        //         matrix[pheromonesHome[cord].x][pheromonesHome[cord].y].pheromones_home = 0;
+        //         updatePixel(ctx, matrix, pheromonesHome[cord].x, pheromonesHome[cord].y);
+        //         delete pheromonesHome[cord];
+        //     }
+        // }
         for (const cord in pheromonesFood) {
-            pheromonesFood[cord].pheromones = pheromonesFood[cord].pheromones * (1 - 0.05);
+            pheromonesFood[cord].pheromones = pheromonesFood[cord].pheromones * (1 - 0.01);
             matrix[pheromonesFood[cord].x][pheromonesFood[cord].y].pheromones_food = pheromonesFood[cord].pheromones;
             updatePixel(ctx, matrix, pheromonesFood[cord].x, pheromonesFood[cord].y);
             if (pheromonesFood[cord].pheromones <= 0) {
@@ -247,17 +314,16 @@ document.addEventListener('DOMContentLoaded', () => {
         const key = `${x},${y}`;
         if (matrix[x][y].pheromones_home === 0) pheromonesHome[key] = {pheromones, x, y};
         else pheromonesHome[key].pheromones += pheromones;
-        ant.nutrition = (1 - 0.01) * ant.nutrition;
+        ant.nutrition = 100 / ant.steps;
         matrix[x][y].pheromones_home += pheromones;
     }
 
     function leavePheromoneFood(ant) {
-        let x = ant.x, y = ant.y, pheromones = ant.nutrition;
+        let x = ant.x, y = ant.y, pheromones = ant.nutrition / ant.steps;
         const key = `${x},${y}`;
         if (matrix[x][y].pheromones_food === 0) pheromonesFood[key] = {pheromones, x, y};
         else pheromonesFood[key].pheromones += pheromones;
-        ant.nutrition = (1 - 0.001) * ant.nutrition;
-        matrix[x][y].pheromones_food += pheromones;
+        matrix[x][y].pheromones_food = Math.max(pheromones, matrix[x][y].pheromones_food);
     }
 
     async function ants_algorithm() {
