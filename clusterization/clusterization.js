c2b7d8370b43ddd16a5161fbc5bc6a4ef9bd7fb6
mkenopsia <gxkurro@gmail.com>
2025-04-07 13:27:24 +0700

    fix points drawing

--- clusterization/clusterization.js
+++ clusterization/clusterization.js
@@ -1,7 +1,9 @@
 // k-means++ + silhouette coefficient
 
 const canvas = document.getElementById('field');
-document.getElementById('start').addEventListener('click', cluserizationAlgo);
+document.getElementById('kmeans').addEventListener('click', kmeans);
+document.getElementById('kmeans++').addEventListener('click', kmeansPlusPlus);
+document.getElementById('DBSCAN').addEventListener('click', dbscan);
 
 const context = canvas.getContext('2d');
 context.strokeRect(0, 0, canvas.width, canvas.height);
@@ -282,7 +284,7 @@ function silhouetteCoef(clusters, centroids) {
     return totalPoints > 0 ? totalScore / totalPoints : -1;
 }
 
-function cluserizationAlgo() {
+function kmeansPlusPlus() {
     let bestK = 1;
     let bestScore = 0;
     let maxK = Math.min(10, points.length);
@@ -290,11 +292,11 @@ function cluserizationAlgo() {
     let bestCentroids = null;
     let k = 1;
     let nums_k = parseInt(document.getElementById('nums_k').value)
-    console.log(nums_k)
+    let flag = false;
     if(!isNaN(nums_k)) {
         k = nums_k;
-        maxK = nums_k += 1;
-
+        maxK = nums_k + 1;
+        flag = true;
     }
     for (; k < maxK; k++) {
         let centroids = getCentroids(k);
@@ -313,7 +315,7 @@ function cluserizationAlgo() {
         
         let silhouetteScore = silhouetteCoef(clusters, centroids);
         
-        if (silhouetteScore > bestScore) {
+        if (silhouetteScore > bestScore || ( flag && maxK - k === 1)) {
             bestScore = silhouetteScore;
             bestK = k;
             bestClusters = clusters;
@@ -321,10 +323,7 @@ function cluserizationAlgo() {
         }
     }
     
-
     draw(bestClusters);
-    // dbscan();
-    // console.log(getDistance(points[0], points[1]));
 }
 
 function epsilonLocalityScan(currPointIndex) {
@@ -337,21 +336,21 @@ function epsilonLocalityScan(currPointIndex) {
     return neighbours;
 }
 
-function expandCluster(currPointIndex, neighbours, visited, clusters) {
+function expandCluster(currPointIndex, neighbours, visited) {
     let currCluster = [];
     currCluster.push(currPointIndex);
-    visited[currPointIndex] = true;
 
     for(let neighbourPointIndex of neighbours) {
         if(!visited[neighbourPointIndex]) {
             visited[neighbourPointIndex] = true;
             let neighbourPointNeighbours = epsilonLocalityScan(neighbourPointIndex);
             if(neighbourPointNeighbours.length >= minPoints) {
-                neighbours.push(...neighbourPointNeighbours);
+                for(let index of neighbourPointNeighbours) {
+                    if(!visited[index]) {
+                        neighbours.push(index);
+                    }
+                }
             }
-        }
-
-        if(!clusters.some(cluster => cluster.includes(neighbourPointIndex)) && !currCluster.includes(neighbourPointIndex)) {
             currCluster.push(neighbourPointIndex);
         }
     }
@@ -371,7 +370,7 @@ function dbscan() {
             visited[i] = true;
             let neighbours = epsilonLocalityScan(i);
             if(neighbours.length >= minPoints) {
-                clusters.push(expandCluster(i, neighbours, visited, clusters));          
+                clusters.push(expandCluster(i, neighbours, visited));          
             }
             else {
                 noises.push(i);
@@ -380,27 +379,122 @@ function dbscan() {
     }
 
 
-    draw(clusters, noises);
+    draw_dbscan(clusters, noises);
     console.log(points);
     console.log(clusters);
     console.log(noises);
 }
 
-// function draw(clusters, noises) {
-//     context.clearRect(1, 1, canvas.width-2, canvas.height-2);
+function draw_dbscan(clusters, noises) {
+    context.clearRect(1, 1, canvas.width-2, canvas.height-2);
     
-//     let i = 0;
+    let i = 0;
+    for (let cluster of clusters) {  
+        context.fillStyle = STYLES[i++];
+        for (let point of cluster) {
+            context.fillRect(points[point].x-5, points[point].y-5, 10, 10);
+        }
+    }
+
+    for (let point of noises) {  
+        context.fillStyle = STYLES[i++];
+        context.fillRect(points[point].x-5, points[point].y-5, 10, 10);
+    }
+
+}
+
+
+function getNormalizedCentroids(centroids, clusters) {
+    for(let i = 0; i < clusters.size; i++) {
+        if(!clusters.has(`${centroids[i].x},${centroids[i].y}`)) {
+            centroids.splice(i, 1);
+        }
+        else {
+            centroids[i] = getNewCentroidCoords(clusters.get(`${centroids[i].x},${centroids[i].y}`))
+        }
+    }
+    return centroids;
+}
+
+function getWcss(map, centroids) {
+    let wcss = 0;
+    let currCentroid = 0;
+    for (const [key, value] of map) {
+        for(const i of map.get(key)) {
+            let dist = getDistance(i, centroids[currCentroid]);
+            wcss += dist*dist;
+        }
+        currCentroid++;
+    }
+    return wcss;
+}
+
+function getRandomCentroids(k) {
+    const left = 0, right = 700;
+    const top = 0, bottom = 600;
+    centroids = [];
+    for(let i = 0; i < k; i++) {
+        const x = (Math.random() * (right - left) + left);
+        const y = (Math.random() * (bottom - top) + top);
+        centroids.push({x: x, y: y});
+    }
+
+    return centroids;
+}
+
+function mapEquation(map1, map2) {
+    if(map1 == null || map2 == null) {
+        return false;
+    }
+    let lengths = [];
+    for (const [key, value] of map1) {
+        lengths.push(value.length);
+    }
+    let i = 0;
+    for (const [key, value] of map2) {
+        if(lengths[i++] != value.length) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function kMeans() {
+    let currCentroids = [];
+    let cluster = null;
+    let wcssHistory = new Map();
+    let wcssValues = []
     
-//     for (let cluster of clusters) {  
-//         context.fillStyle = STYLES[i++];
-//         for (let point of cluster) {
-//             context.fillRect(points[point].x-5, points[point].y-5, 10, 10);
-//         }
-//     }
+    for(let k = 1; k < 15; k++) {
+        currCentroids = getRandomCentroids(k);
+        let currCluster = centralize(currCentroids);
+        while(!mapEquation(cluster, currCluster)) {
+            cluster = currCluster;
+            currCentroids = getNormalizedCentroids(currCentroids, currCluster);
+            currCluster = centralize(currCentroids);
+        }
+        let currWcss = getWcss(currCluster, currCentroids);
+        wcssValues.push(currWcss);
+        wcssHistory.set(currWcss, currCluster);
+        // console.log(currWcss);
 
-//     for (let point of noises) {  
-//         context.fillStyle = STYLES[i++];
-//         context.fillRect(points[point].x-5, points[point].y-5, 10, 10);
-//     }
+        cluster = null;
+    }
 
-// }
+    let index = 0;
+    let maxDiff = 0;
+    for(let i = 1; i < wcssValues.length - 1; i++) {
+        let currDiff = wcssValues[i-1] - wcssValues[i] + (wcssValues[i] - wcssValues[i]);
+        if(currDiff > maxDiff) {
+            index = i;
+            maxDiff = currDiff;
+        }
+    }
+
+    let result = wcssHistory.get(wcssValues[index]);
+
+    console.log(wcssHistory);
+
+    draw(result);
+
+}
