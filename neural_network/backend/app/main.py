d76b6d302b48042a5920f1f7491cf89aa160770c
aspakaramych <vgrig2006@gmail.com>
2025-04-10 17:59:43 +0700

    change nn weights

--- neural_network/backend/app/main.py
+++ neural_network/backend/app/main.py
@@ -3,9 +3,13 @@ import numpy as np
 from fastapi import FastAPI, Body
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
-from .nn.neural_network import predict
+from .nn.neural_network import predict, create_model
+
+PATH = "./app/weights/weights.json"
 
 app = FastAPI()
+model = create_model(PATH)
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=['*']
@@ -17,8 +21,9 @@ async def get_prediction(data=Body()):
     decoded_data = data.decode("utf-8")
     parsed_data = json.loads(decoded_data)
     num_array = np.array(parsed_data["data"]).astype(np.uint8)
-    num = predict(num_array, "./app/weights/weights.json")
+    num = predict(num_array, model)
     return num
 
+
 if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=80)
+    uvicorn.run(app, host='0.0.0.0', port=8000)
