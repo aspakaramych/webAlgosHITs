ca8e8db0e60dcb9d555cdae0a1f33d06b73c2e44
mkenopsia <gxkurro@gmail.com>
2025-04-19 00:37:46 +0700

    add email service (beta)

---
+++ email/src/main/java/com/emailservice/email/service/MailSenderService.java
@@ -0,0 +1,29 @@
+package com.emailservice.email.service;
+
+import com.emailservice.email.controller.payload.EmailPayload;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailSenderService {
+
+    private final JavaMailSender mailSender;
+
+    @Autowired
+    public MailSenderService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void send(EmailPayload emailPayload) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo("gxkurro@yandex.ru");
+        mailMessage.setSubject("New algorithm suggestion");
+        mailMessage.setText(emailPayload.text());
+        mailMessage.setFrom("gxkurro@yandex.ru");
+
+        mailSender.send(mailMessage);
+    }
+
+}
