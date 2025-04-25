8f16c5b5c38b97ce4902ea21ae7598901f3eec37
dmitryLem <dlemesonok0@gmail.com>
2025-04-25 19:28:37 +0700

    separate js to some files

--- genetic/script/geneticAlgorithm.js
+++ genetic/script/geneticAlgorithm.js
@@ -1,26 +1,22 @@
+import {fitness, halfUniformCrossover, selectParent, distHamming, generatePopulation, greedyAlgorithm,
+    inversionMutation, cataclysmicMutation} from './geneticEngineering.js'
+
+import {cntPopulation, render, cntPairs, cntEpoch, radDots, mutationRate, inbreeding,
+    thresholdStagnation} from './config.js'
+
+//массивы для хранения информации о точках
 let dots = [];
-let divs_dots = [];
+let divsDots = []
+
+//переменные для асинхронной работы алгоритма
 let isProcessing = false;
 let controller = new AbortController();
-
-let cnt_population = 1000;
-let cnt_epoch = 10000;
-let mutation_rate = 0.5;
-let tournament_size = 20;
-let cnt_pairs = 500;
-let threshold_stagnation = 100;
-let inbreeding = 0.1;
-
-let render = 1;
-
-function delay(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
 
 function stopAlgorithm() {
     controller.abort();
 }
 
+//считает расстояние между всеми городами, возвращает матрицу смежности
 function countDistances(arrayTowns) {
     const n = arrayTowns.length;
     const dist = Array.from({ length: n }, () => Array(n).fill(0));
@@ -34,160 +30,32 @@ function countDistances(arrayTowns) {
     return dist;
 }
 
-function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-}
-
-function fitness(dist, way) {
-    let cnt = 0;
-    for (let i = 1; i < way.length; i++) {
-        cnt += dist[way[i - 1]][way[i]];
-    }
-    cnt += dist[way[way.length - 1]][way[0]]
-    return cnt;
-}
-
-function mutation(array) {
-    const i = Math.floor(Math.random() * (array.length));
-    const j = Math.floor(Math.random() * (array.length));
-    [array[i], array[j]] = [array[j], array[i]];
-}
-
-function otherMutation(array) {
-    const start = Math.floor(Math.random() * (array.length));
-    const end = Math.floor(Math.random() * (array.length - start)) + start;
-    for (let i = start; i < (end - start) / 2; i++) {
-        [array[i], array[end - i + start]] = [array[end - i + start], array[i]];
-    }
-}
-
-// алгоритм кроссовера по Эшелману, берём по половине генов от обоих родителей
-function halfUniformCrossover(parent1, parent2) {
-    const length = parent1.length;
-    const lengthPar1 = Math.floor(parent1.length * Math.random());
-    let child = Array(length).fill(null);
-    let used = new Set();
-
-    const start = Math.floor(Math.random() * (lengthPar1 / 2));
-    const end = start + lengthPar1 / 2;
-
-    for (let i = start; i < end; i++) {
-        child[i - start]  = parent1[i];
-        used.add(parent1[i]);
-    }
-
-    let ptr = 0;
-    for (let i = 0; i < length; i++) {
-        if (child[i] === null) {
-            while (used.has(parent2[ptr])) {
-                ptr++;
-            }
-            child[i] = parent2[ptr];
-            used.add(parent2[ptr]);
-        }
-    }
-
-    if (Math.random() < mutation_rate){
-        mutation(child);
-    }
-
-    return child;
-}
-
-//выбор особи турнирным способом
-function selectParent(population, dist) {
-    let candidates = [];
-    for (let i = 0; i < tournament_size; i++) {
-        candidates.push(population[Math.floor(Math.random() * (population.length - 1))]);
-    }
-
-    let winner = 0;
-    let min_fitness = fitness(dist, candidates[0]);
-    for (let i = 1; i < candidates.length; i++) {
-        let temp = fitness(dist, candidates[i])
-        if (temp < min_fitness) {
-            winner = i;
-            min_fitness = temp;
-        }
-    }
-    return candidates[winner];
-}
-
-//расстояние хэмминга (различие между родителями) для предотвращения инбридинга
-function distHamming(parent1, parent2) {
-    let distance = 0;
-    for (let i = 0; i < parent1.length; i++) {
-        if (parent1[i] !== parent2[i]) {
-            distance++;
-        }
-    }
-    return distance;
-}
-
-function greedyAlgorithm(dist, start) {
-    let way = [];
-    let used = new Set();
-    used.add(start);
-    way.push(start);
-    let last = start;
-    for (let i = 0; i < dist.length - 1; i++) {
-        let min = Number.MAX_VALUE;
-        let ver = -1;
-        for (let j = 0; j < dist[last].length; j++) {
-            if (last !== j) {
-                if (min > dist[last][j] && !used.has(j)) {
-                    ver = j;
-                    min = dist[last][j];
-                }
-            }
-        }
-        last = ver;
-        way.push(ver);
-        used.add(ver);
-    }
-    return way;
-}
-
-function generatePopulation(cntIndivid, numTowns) {
-    let rightOrder = Array.from({length: numTowns}, (_, index) => index);
-    let population = [];
-    for (let i = 0; i < cntIndivid; i++) {
-        shuffleArray(rightOrder);
-        population.push([...rightOrder]);
-    }
-    return population;
-}
-
-function cataclysmicMutation(population) {
-    for (let i = 1; i < 10; i++) {
-        population[i] = halfUniformCrossover(population[0], population[i]);
-    }
-    for (let i = 1; i < population.length; i++) {
-        shuffleArray(population[i]);
-    }
-}
-
 document.addEventListener('DOMContentLoaded', () => {
-    const table = document.getElementById('table');
+    //элементы для взаимодействия
+    const plane = document.getElementById('plane');
     const startButton = document.getElementById('startButton');
     const resetButton = document.getElementById('resetButton');
     const linesSvg = document.getElementById('lines');
+    
+    //окно с ошибкой
     const errorModal = document.getElementById('error-modal');
     const closeErrorModal = document.getElementById('close-error-modal');
     const parentContainer = document.getElementById('parent-container');
 
-    table.addEventListener('click', (event) => {
-        const rect = table.getBoundingClientRect();
+    //расставляем точки на плоскости
+    plane.addEventListener('click', (event) => {
+        const rect = plane.getBoundingClientRect();
         const x = event.clientX - rect.left;
         const y = event.clientY - rect.top;
+        
+        //проверяем есть ли в этой зоне уже точка
         for (let i = 0; i < dots.length; i++) {
-            if (Math.abs(dots[i].x - x) <= 8 && Math.abs(dots[i].y - y) <= 8) {
-                divs_dots[i].remove();
-                divs_dots.splice(i, 1);
+            if (Math.abs(dots[i].x - x) <= radDots && Math.abs(dots[i].y - y) <= radDots) {
+                divsDots[i].remove();
+                divsDots.splice(i, 1);
                 dots.splice(i, 1);
+
+                //если алгоритм работает, перезапускаем
                 if (isProcessing) {
                     stopAlgorithm();
                     setTimeout(() => {
@@ -198,23 +66,29 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         }
 
-        const dot = document.createElement('div');
+        //добавление точки на плокскость
+        let dot = document.createElement('div');
         dot.className = 'dot';
         dot.style.left = `${x}px`;
         dot.style.top = `${y}px`;
-
-        table.appendChild(dot);
-        divs_dots.push(dot);
+        plane.appendChild(dot);
+        
+        divsDots.push(dot);
 
         dots.push({x, y});
+
+        //если алгоритм работает, перезапускаем
         if (isProcessing) {
             stopAlgorithm();
             setTimeout(() => {
                 geneticAlgorithm();
-            }, render * 10);
+                startButton.textContent = 'Остановить процесс поиска';
+                startButton.style.backgroundColor = '#e46262';
+            }, render * 50);
         }
     })
 
+    //кнопка запуска и остановки
     startButton.addEventListener('click', () => {
         if (isProcessing) {
             startButton.textContent = 'Поиск оптимального пути';
@@ -223,11 +97,10 @@ document.addEventListener('DOMContentLoaded', () => {
             startButton.style.backgroundColor = 'springgreen';
         }
         else {
-            if (dots.length < 2) {
+            if (dots.length < 2) { //если точек мало, чтобы построить путь, выкидывем модальное окно
                 errorModal.style.display = 'flex';
                 parentContainer.classList.add('blur');
             } else {
-                //generateRandomDots(200, 600, 600);
                 geneticAlgorithm();
                 startButton.textContent = 'Остановить процесс поиска';
                 startButton.style.backgroundColor = '#e46262';
@@ -235,10 +108,12 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     })
 
+    //перезагрузка (очистка) поля
     resetButton.addEventListener('click', () => {
         resetField();
     });
 
+    //уйти из модального окна
     closeErrorModal.addEventListener('click', () => {
         errorModal.style.display = 'none';
         parentContainer.classList.remove('blur');
@@ -249,7 +124,7 @@ document.addEventListener('DOMContentLoaded', () => {
             stopAlgorithm();
         }
         dots = [];
-        divs_dots = [];
+        divsDots = [];
 
         const existingDots = document.querySelectorAll('.dot');
         existingDots.forEach(dot => dot.remove());
@@ -260,6 +135,7 @@ document.addEventListener('DOMContentLoaded', () => {
         startButton.style.backgroundColor = 'springgreen';
     }
 
+    //функция для отладки, генерирует n случайных точек
     function generateRandomDots(count, width, height) {
         dots = [];
         for (let i = 0; i < count; i++) {
@@ -271,9 +147,9 @@ document.addEventListener('DOMContentLoaded', () => {
             dot.className = 'dot';
             dot.style.left = `${x}px`;
             dot.style.top = `${y}px`;
-            table.appendChild(dot);
+            plane.appendChild(dot);
         }
-        drawLines();
+        drawIndividual();
     }
 
     function addLine(dot1, dot2) {
@@ -287,7 +163,8 @@ document.addEventListener('DOMContentLoaded', () => {
         linesSvg.appendChild(line);
     }
 
-    function drawLines(seqDots = Array.from({length: dots.length}, (_, index) => index)) {
+    //для отрисовки пути
+    function drawIndividual(seqDots = Array.from({length: dots.length}, (_, index) => index)) {
         linesSvg.innerHTML = '';
 
         if (dots.length > 1) {
@@ -299,61 +176,77 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     async function geneticAlgorithm() {
+        //для асинхронности
         isProcessing = true;
+        controller = new AbortController();
+
         let dist = countDistances(dots);
-        let population = generatePopulation(cnt_population, dots.length);
+        let population = generatePopulation(cntPopulation, dots.length);
+
+        //генерируем какое-то количество особей с помощью жадного алгоритма, ответ скорее всего не среди них, но в них
+        //точно есть оптимизированные участки пути
         for (let i = 0; i < dist.length; i++) {
             population.push(greedyAlgorithm(dist, i));
         }
-        drawLines(population[0]);
-        controller = new AbortController();
 
+        //отрисовываем какую-то особь
+        drawIndividual(population[0]);
+
+        //основной алгоритм для просчёта поколений
         async function alghorythm() {
             let stagnation = 0;
             let elite = population[0];
 
-            for (let i = 0; i < cnt_epoch; i++) {
-                if (controller.signal.aborted) {
+            for (let i = 0; i < cntEpoch; i++) {
+                if (controller.signal.aborted) { //проверка контроллер на остановку
                     console.log("Алгоритм остановлен");
                     return;
                 }
+
+                //ранжируем особей по приспособленности
                 population.sort((a, b) => fitness(dist, a) - fitness(dist, b));
 
-                population = population.slice(0, cnt_population);
+                //выкидываем из популяции лишних особей
+                population = population.slice(0, cntPopulation);
+
+                //проверка на смену лучшей особи
                 if (population[0] === elite) stagnation++;
-                else stagnation = 0;
 
                 elite = population[0];
 
-                if (stagnation > threshold_stagnation) {
+                if (stagnation > thresholdStagnation) {
                     stagnation = 0;
                     cataclysmicMutation(population);
                 } else {
-                    for (let j = 0; j < cnt_pairs; j++) {
+                    for (let j = 0; j < cntPairs; j++) {
                         if (controller.signal.aborted) {
                             console.log("Алгоритм остановлен");
                             return;
                         }
+
+                        //выбираем родителей
                         let parent1 = selectParent(population, dist);
                         let parent2 = selectParent(population, dist);
+
+                        //проверяем родителей на близкородственность
                         if (distHamming(parent1, parent2) > Math.floor((parent1.length - 1) * inbreeding)) {
+                            //получаем 2 детей
                             population.push(halfUniformCrossover(parent1, parent2));
                             population.push(halfUniformCrossover(parent2, parent1));
                         }
                     }
                 }
 
-                for (let j = 10; j < 10 + Math.floor(mutation_rate * cnt_population); j++) {
-                    otherMutation(population[j]);
+                //мутиируем какую-то часть популяции
+                for (let j = 10; j < 10 + Math.floor(mutationRate * cntPopulation); j++) {
+                    inversionMutation(population[j]);
                     if (controller.signal.aborted) {
                         console.log("Алгоритм остановлен");
                         return;
                     }
                 }
 
-                drawLines(elite);
-
-                console.log(fitness(dist, elite));
+                drawIndividual(elite);
 
                 await new Promise((resolve, reject) => {
                     const timeout = setTimeout(resolve, render);
@@ -366,14 +259,9 @@ document.addEventListener('DOMContentLoaded', () => {
             console.log('Алгоритм завершен');
         }
 
-        try {
-            await alghorythm();
-        } catch (error) {
-            console.log("Алгоритм остановлен:", error.message);
-            startButton.style.backgroundColor = 'springgreen';
-            startButton.textContent = 'Поиск оптимального пути';
-        } finally {
-            isProcessing = false;
-        }
+        await alghorythm();
+        isProcessing = false;
+        startButton.style.backgroundColor = 'springgreen';
+        startButton.textContent = 'Поиск оптимального пути';
     }
 })
